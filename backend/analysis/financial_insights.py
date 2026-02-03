4c1d3ce3bee79b401420e1c142b8b09176064e2d
M-vignesh08 <m.vignesh1408@gmail.com>
2026-02-03 23:50:14 +0530

    Add files via upload

---
+++ backend/analysis/financial_insights.py
@@ -0,0 +1,11 @@
+def generate_trends(df, revenue_col, expense_col):
+    return {}
+
+def generate_risk_flags(basic_metrics, cashflow_metrics):
+    return []
+
+def generate_recommendations(risk_flags):
+    return []
+
+def explain_health_score(basic_metrics, cashflow_metrics):
+    return []
