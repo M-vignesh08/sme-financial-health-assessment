eb03c100ca2fc0b46faaf21a99fd4f53789f3b89
M-vignesh08 <m.vignesh1408@gmail.com>
2026-02-03 16:20:39 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 import shutil
 import os
 
-from utils.file_parser import parse_financial_file
-from models.schemas import UploadResponse
+from backend.utils.file_parser import parse_financial_file
+from backend.models.schemas import UploadResponse
+
 
 app = FastAPI(title="SME Financial Health Assessment API")
+
